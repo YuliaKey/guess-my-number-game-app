d3bc76ac7c087895da0b104f074814ff9d20d409
Yuliia Kolomiiets <jk182013@gmail.com>
2024-09-26 18:13:21 +0200

    feat: add number input and reusable button component

---
+++ components/PrimaryButton.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, View, Pressable } from "react-native";
+
+export type ButtonProps = {
+  name: string;
+  onPress: () => void;
+};
+
+const PrimaryButton = ({ name, onPress }: ButtonProps) => {
+  const handlePressButton = () => {
+    console.log("Pressed");
+    onPress();
+  };
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={handlePressButton}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) => [
+          styles.buttonInnerContainer,
+          pressed && styles.pressed,
+        ]}
+      >
+        <Text style={styles.buttonText}>{name}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, TextInput, View } from "react-native";
+
+import PrimaryButton from "@/components/PrimaryButton";
+
+export default function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <PrimaryButton onPress={() => {}} name="Reset" />
+      <PrimaryButton onPress={() => {}} name="Confirm" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: "bold",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    textAlign: "center",
+  },
+});
